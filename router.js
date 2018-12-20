1a0cebf38c56848336dd37a4b80afb102ff7d302
842mono <842mono@gmail.com>
2018-12-20 13:12:50 +0200

    login

--- router.js
+++ router.js
@@ -7,24 +7,75 @@ let User = require('./Models/User');
 let Attendance = require('./Models/Attendance');
 let Status = require('./Models/Status');
 
+let controller = require("./controller");
+
+router.post('/plainlogin', (req, res) =>
+{
+    console.log(req.body.usernamein);
+    console.log(req.body.passwordin);
+    
+    User.findOne
+    (
+        {Name:req.body.usernamein, Password:req.body.passwordin},
+        (err, user) =>
+        {
+            console.log(user);
+            if(err || user == null)
+                res.sendFile("/Views/index.html", {root: __dirname });
+            else
+            {
+                res.send("success");
+            }
+        }
+    );
+});
+
+router.get('/', (req, res) =>
+{
+    res.sendFile("/Views/index.html", {root: __dirname });
+});
+
 router.get('/test', (req, res) =>
 {
-    res.send("hello");
+    res.send("working");
 });
 
 router.get('/populate', (req, res) =>
 {
-    let TestEmployee = new Employee
+    // let TestEmployee = new Employee
+    // ({
+    //     Name:"user1",
+    //     Email:"user1@user1.com",
+    //     MobileNumber:"1111",
+    //     HireDate:new Date()
+    // });
+
+    // TestEmployee.save(err => {if(err) console.log(err)});
+
+    let TestUser = new User
     ({
-        Name:"user1",
-        Email:"user1@user1.com",
-        MobileNumber:"1111",
-        HireDate:new Date()
+        Name:"U1",
+        Email:"U1@U1.U1",
+        Password:"passU1"
     });
 
-    TestEmployee.save(err => {if(err) console.log(err)});
+    TestUser.save(err => {if(err) console.log(err)});
 
     res.send("done");
+});
+
+router.get('/testsession', function(req, res)
+{
+    console.log(res.session);
+    console.log(req.session);
+    // res.send(req.session);
+    // if(req.session)
+    //     console.log(req.session.test);
+    if(!req.session || req.session.test == null)
+        req.session.test = 0
+    else
+        req.session.test = parseInt(req.session.test) + 1;
+    res.send(req.session);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 let express = require('express');
-let app = express();
+let portal = express();
 let router = require('./router');
+let bodyparser = require('body-parser');
+
+var session = require('express-session');
 
 let mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/eseedInterview");
 
 
-app.use('/api', router);
-app.listen(5000, function(){console.log("Serving on 5000")});
+portal.use(bodyparser.json());
+portal.use(bodyparser.urlencoded({extended:false}));
+portal.use(session({ resave:true, saveUninitialized:true, secret:'randomstring'}));
+portal.use(router);
+portal.listen(5000, function(){console.log("Serving on 5000")});
