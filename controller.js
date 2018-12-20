d0815e2e6713e4fa34906f353fdc29c9a585541b
842mono <842mono@gmail.com>
2018-12-20 13:47:23 +0200

    table of employees

--- controller.js
+++ controller.js
@@ -1,4 +1,17 @@
+let Employee = require('./Models/Employee');
+
 module.exports = 
 {
-
+    GetEmployees:() =>
+    {
+        Employee.find
+        (
+            {},
+            (err, employees) =>
+            {
+                // return emplo
+            }
+        );
+        return "e";
+    }
 }
--- router.js
+++ router.js
@@ -24,7 +24,61 @@ router.post('/plainlogin', (req, res) =>
                 res.sendFile("/Views/index.html", {root: __dirname });
             else
             {
-                res.send("success");
+                Employee.find
+                (
+                    {},
+                    (err, employees) =>
+                    {
+                        if(err)
+                            console.log(err);
+                        // res.send(employees);
+
+                        let Template = `<html>
+                        <head>
+                            <title>User Page</title>
+                        </head>
+                        <body>
+                            Welcome to the user page.
+                    
+                            <br/>
+                            <br/>
+                            Employees:
+                            <table style="width:100%; border: 1px solid black;">
+                                <tr>
+                                  <th>Name</th>
+                                  <th>Email</th>
+                                  <th>Mobile Phone</th>
+                                  <th>Hire Date</th>
+                                  <th>Edit</th>
+                                  <th>Delete</th>
+                                </tr>`;
+
+                                console.log(employees[1].HireDate)
+                        
+                        for(let i = 0; i < employees.length; ++i)
+                        {
+                            let dt = new Date(employees[i].HireDate);
+                            let m = parseInt(dt.getMonth());
+                            let day = parseInt(dt.getDay());
+                            let year  = parseInt(dt.getFullYear());
+                            Template += `            <tr>
+                            <td><input type="text" value="${employees[i].Name}"></td>
+                            <td><input type="text" value="${employees[i].Email}"></td> 
+                            <td><input type="text" value="${employees[i].MobileNumber}"></td>
+                            <td><input type="date" value="${year}-${m}-${day}"></td>
+                            <td><button type="submit">Edit</button></td>
+                            <td><button type="submit">Delete</button></td>
+                          </tr>`;
+                        }
+
+                        Template += `          </table>
+                        </body>
+                    </html>`
+
+                        res.send(Template);
+                    }
+                );
+                // res.send(controller.GetEmployees());
             }
         }
     );
